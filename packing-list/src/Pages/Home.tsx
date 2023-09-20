17547cf11e95f601e85369a47071becb21c6e36e
Theojeddy <Theojeddy@hotmail.co.uk>
2023-09-20 16:18:55 +0100

    initialised app, removed basic template, begun with home page and checkbox component created

---
+++ packing-list/src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../App.css";
+import Checkbox from "../components/Checkbox";
+
+export default function Home() {
+  return (
+    <div className="App">
+        <p>hello</p>
+        <Checkbox/>
+      
+    </div>
+  );
+};
---
+++ packing-list/src/components/Checkbox.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Checkbox() {
+  return <div className="App">
+    <p>hello checkbox</p>
+  </div>;
+}
